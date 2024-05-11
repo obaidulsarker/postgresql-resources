10ca68460ef4241f9ce57c4c6d905ff3662bfd14
Md. Obaidul Haque Sarker <aupo37@gmail.com>
2024-05-11 13:26:12 +0600

    add backup remove scripts

---
+++ remove-dump-backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+LOC=/backup;
+LOGLOC=$LOC;
+LOGFILE=$LOGLOC/removeBackup.log;
+DBNAME=db1;
+touch $LOGFILE;
+
+FILES=`find $LOC/ -type f -mtime +15 | grep $DBNAME | sort`;
+
+for n in $FILES;
+do
+        /bin/rm -rf $n;
+        if [ -f $n ]; then
+                echo "File : $n Removed Failed # $(date)" >> $LOGFILE;
+        else
+                echo "File : $n Removed # $(date)" >> $LOGFILE;
+        fi
+done
+
+exit
